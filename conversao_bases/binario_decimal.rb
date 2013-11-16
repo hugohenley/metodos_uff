1f0003d427e9fbbe3005168413c911ec165b8e85
Hugo Henley <hugohenley@gmail.com>
2013-11-16 14:57:11 -0200

    Corrigindo problema com refactoring no binario decimal

--- conversao_bases/binario_decimal.rb
+++ conversao_bases/binario_decimal.rb
@@ -3,17 +3,17 @@ class BinarioDecimal
   attr_accessor :binario
 
   def initialize(binario)
-    @octa = binario
+    @binario = binario
   end
 
   def to_decimal
-    return 1 if octa == 1
-    tamanho = @octa.to_s.size
+    return 1 if binario == 1
+    tamanho = @binario.to_s.size
     indice = 0
     soma = 0
     while tamanho != 0 do
-      soma += ((@octa % 10) * (2**indice))
-      @octa = @octa / 10
+      soma += ((@binario % 10) * (2**indice))
+      @binario = @binario / 10
       indice += 1
       tamanho -= 1
     end
