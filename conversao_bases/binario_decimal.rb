803a34b9fa43d0dfb94a3723fb2ced3709829a90
Hugo Henley <hugohenley@gmail.com>
2013-11-16 14:43:55 -0200

    Conversão de binário para decimal

--- conversao_bases/binario_decimal.rb
+++ conversao_bases/binario_decimal.rb
@@ -7,8 +7,17 @@ class BinarioDecimal
   end
 
   def to_decimal
-    return '1' if binario == 1
+    return 1 if binario == 1
+    tamanho = @binario.to_s.size
+    indice = 0
+    soma = 0
+    while tamanho != 0 do
+      soma += ((@binario % 10) * (2**indice))
+      @binario = @binario / 10
+      indice += 1
+      tamanho -= 1
+    end
+    soma
   end
-
 
 end
