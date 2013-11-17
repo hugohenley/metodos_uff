1afe8124f83bfa682a3222effa1cd891412d72d4
Hugo Henley <hugohenley@gmail.com>
2013-11-17 16:42:21 -0200

    Correção na interface com o usuário

--- conversao_bases/conversor.rb
+++ conversao_bases/conversor.rb
@@ -2,6 +2,7 @@
 require '../conversao_bases/conversao_decimal'
 require '../conversao_bases/decimal_outra'
 require '../conversao_bases/decimal_hexa'
+require '../conversao_bases/hexa_decimal'
 
 puts "Escolha um número para converter"
 numero = gets.chomp
@@ -14,6 +15,10 @@ decimal = ConversaoDecimal.new(numero, base).to_decimal
 if nova_base == '16'
   puts "O número #{numero} da base #{base} para a base #{nova_base} é:"
   puts DecimalHexa.new(decimal).to_hex
+elsif base == '16'
+  puts "O número #{numero} da base #{base} para a base #{nova_base} é:"
+  decimal = HexaDecimal.new(numero).to_decimal
+  puts DecimalOutra.new(decimal, nova_base).converter
 else
   puts "O número #{numero} da base #{base} para a base #{nova_base} é:"
   puts DecimalOutra.new(decimal, nova_base).converter
